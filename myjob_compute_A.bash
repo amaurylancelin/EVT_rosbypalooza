0649fa97f5dca4218de0957351641241057218fb
Karan Jakhar <kjakhar@derecho1.hsn.de.hpc.ucar.edu>
2024-07-26 07:57:02 -0600

    clusture scripts added

---
+++ myjob_compute_A.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+var=tas
+lat=46
+lon=6
+reduce=max
+
+# Chicago - lat = 41, lon = -88
+# China - lat=34.00, lon=109.00
+# India - lat=27, lon=72
+# NW US - lat=45, lon=-120
+# Saudi - lat=25, lon=47
+# Europe - lat=48, lon=2
+
+for sim in 0 1 2
+# for sim in 0 
+do
+    qsub -v var=$var,sim_number=$sim,reduce=$reduce,lat=$lat,lon=$lon myjob_compute_A.pbs
+    echo $i
+done
