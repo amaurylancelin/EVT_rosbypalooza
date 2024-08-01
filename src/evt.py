c0973a065fd08699d0aaff3dadee4dbdb0effc21
Theo <kcarr@whoi.edu>
2024-08-01 10:04:40 -0500

    remove 1st mode of distribution

--- src/evt.py
+++ src/evt.py
@@ -9,7 +9,7 @@ rng = np.random.default_rng()
 
 
 def fit_model(data, model_class, bounds):
-    """Get model fitted to data. Returns scipy.rv_continuous object. 
+    """Get model fitted to data. Returns scipy.rv_continuous object.
     Bounds should be a dictionary with keys 'c', 'loc', 'scale'"""
 
     ## First, estimate parameters
@@ -41,7 +41,9 @@ def draw_sample(data, n=None):
     return rng.choice(data, size=n, replace=True)
 
 
-def load_return_period_bnds(data, model_class, save_fp, bounds, n_samples=1000, alpha=0.05, MC=False, **kwargs):
+def load_return_period_bnds(
+    data, model_class, save_fp, bounds, n_samples=1000, alpha=0.05, MC=False, **kwargs
+):
     """load in or compute return period bounds"""
     if os.path.isfile(save_fp):
 
@@ -53,24 +55,29 @@ def load_return_period_bnds(data, model_class, save_fp, bounds, n_samples=1000, 
         ub = bounds["ub"]
     else:
         ## compute bounds
-        if not MC: 
+        if not MC:
             lb, ub = compute_return_period_bnds(
-            data, model_class=model_class, n_samples=n_samples, alpha=alpha, bounds=bounds)
+                data,
+                model_class=model_class,
+                n_samples=n_samples,
+                alpha=alpha,
+                bounds=bounds,
+            )
             ## save to file
             with open(save_fp, "wb") as file:
                 pickle.dump({"lb": lb, "ub": ub}, file)
         else:
-            mean, median, lb, ub = compute_MC_return_period_bnds(data, model_class=model_class, bounds=bounds, alpha=alpha, **kwargs)
+            mean, median, lb, ub = compute_MC_return_period_bnds(
+                data, model_class=model_class, bounds=bounds, alpha=alpha, **kwargs
+            )
             ## save to file
             with open(save_fp, "wb") as file:
                 pickle.dump({"mean": mean, "median": median, "lb": lb, "ub": ub}, file)
 
     if MC:
         return mean, median, lb, ub
-    else: 
+    else:
         return lb, ub
-    
-
 
 
 def compute_return_period_bnds(data, model_class, bounds, n_samples=1000, alpha=0.05):
@@ -96,7 +103,10 @@ def compute_return_period_bnds(data, model_class, bounds, n_samples=1000, alpha=
 
     return lb, ub
 
-def compute_MC_return_period_bnds(full_data, model_class, bounds, alpha=0.05, n_train=80, n_mc=10):
+
+def compute_MC_return_period_bnds(
+    full_data, model_class, bounds, alpha=0.05, n_train=80, n_mc=10
+):
     """get bounds for return period using Monte Carlo sampling"""
     ## empty list to hold result
     return_levels_samples = []
@@ -112,8 +122,12 @@ def compute_MC_return_period_bnds(full_data, model_class, bounds, alpha=0.05, n_
     return_levels_samples = np.stack(return_levels_samples, axis=0)
     lb, ub = np.quantile(return_levels_samples, axis=0, q=[alpha / 2, 1 - alpha / 2])
     # return mean, median, lb, ub
-    return np.mean(return_levels_samples, axis=0), np.median(return_levels_samples, axis=0), lb, ub
-        
+    return (
+        np.mean(return_levels_samples, axis=0),
+        np.median(return_levels_samples, axis=0),
+        lb,
+        ub,
+    )
 
 
 def get_empirical_pdf(data, bin_edges=None):
@@ -121,8 +135,12 @@ def get_empirical_pdf(data, bin_edges=None):
 
     ## Set bin_edges if unspecified
     if bin_edges is None:
-        print(f"Number of bins not specified. Using square root rule. {np.sqrt(len(data)).astype(int)}")
-        bin_edges = np.linspace(np.floor(data.min()), np.ceil(data.max()), np.sqrt(len(data)).astype(int))
+        print(
+            f"Number of bins not specified. Using square root rule. {np.sqrt(len(data)).astype(int)}"
+        )
+        bin_edges = np.linspace(
+            np.floor(data.min()), np.ceil(data.max()), np.sqrt(len(data)).astype(int)
+        )
 
     ## make histogram
     counts, _ = np.histogram(data, bins=bin_edges)
@@ -148,9 +166,10 @@ def get_empirical_return_period(X):
     return tr_empirical, Xr_empirical
 
 
-def compute_MC_return_value(full_data, model_class, bounds, n_train=80, n_mc=10, return_periods=100):
-    """Compute the return value at a target return period using Monte Carlo simulation
-    """
+def compute_MC_return_value(
+    full_data, model_class, bounds, n_train=80, n_mc=10, return_periods=100
+):
+    """Compute the return value at a target return period using Monte Carlo simulation"""
     ## empty list to hold result
     return_levels_samples = []
     for i in tqdm(range(n_mc)):
@@ -162,20 +181,23 @@ def compute_MC_return_value(full_data, model_class, bounds, n_train=80, n_mc=10,
         else:
             model = fit_model(train_data, model_class=model_class, bounds=bounds)
         # compute return period
-        return_levels_samples.append(get_return_levels(model, return_periods=return_periods)[0])
+        return_levels_samples.append(
+            get_return_levels(model, return_periods=return_periods)[0]
+        )
 
     ## convert to array and compute bounds
     return_levels_samples = np.stack(return_levels_samples, axis=0)
     return return_levels_samples
 
+
 def get_target_return_value(X_test, target_return_period):
-    """Interpolate the return value at a target return period with the 
+    """Interpolate the return value at a target return period with the
     empirical return periods of the whole ground truth"""
-    
+
     t_test_r_empirical, X_test_r_empirical = get_empirical_return_period(X_test)
 
     index_target_rv = np.argmin(np.abs(t_test_r_empirical - target_return_period))
     if t_test_r_empirical[index_target_rv] < target_return_period:
         index_target_rv += 1
-    target_rv = np.mean(X_test_r_empirical[index_target_rv-1:index_target_rv+1])
+    target_rv = np.mean(X_test_r_empirical[index_target_rv - 1 : index_target_rv + 1])
     return target_rv, index_target_rv
